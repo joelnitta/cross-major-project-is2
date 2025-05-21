fe56a8f94562160f8ef2bcdbb51dd227d0061d52
joelnitta <joelnitta@gmail.com>
2025-05-22 08:47:43 +0900

    Add cleaning code for 2025

--- data_cleaning.R
+++ data_cleaning.R
@@ -46,3 +46,37 @@ read_csv(
   ) |>
   # write out
   write_csv("data/okujo_2024.csv")
+
+# 2025
+read_csv(
+  "data/屋上はらっぱ植生調査 - 2025 とりまとめ.csv",
+  show_col_types = FALSE,
+  col_names = c(
+    "quadrat",
+    "name_ja",
+    "name_en",
+    "cover",
+    "abun",
+    "pop"
+  )
+) |>
+  # remove rows where every column is empty
+  filter(!if_all(everything(), is.na)) |>
+  filter(!if_all(c(name_en, cover, abun, pop), is.na)) |>
+  filter(!is.na(quadrat)) |>
+  # remove rows that actually contain metadata about year of collection
+  filter(!str_detect(quadrat, "区画")) |>
+  # split quadrat into plot and quadrat
+  separate_wider_delim(
+    quadrat,
+    delim = "-",
+    names = c("plot", "quadrat")
+  ) |>
+  # remove rows with no cover, abun, or pop data
+  filter(!(is.na(cover) & is.na(abun) & is.na(pop))) |>
+  # replace JA font numbers with ASCII, convert to numeric
+  mutate(
+    across(cover:pop, ~ str_replace_all(.x, ja_en_nums) |> parse_number())
+  ) |>
+  # write out
+  write_csv("data/okujo_2025.csv")
